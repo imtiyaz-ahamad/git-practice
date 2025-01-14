0a950d1e34f529e2b1895a634513b97a0424aeac
Imtiyaz <imtiyaz.ahamad.work@gmail.com>
2025-01-14 18:23:28 +0530

    temp commit to local

--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@ class Demo{
  System.out.println("Hello world2");
  System.out.println("Hello World3");
  System.out.println("Hello World4");
+
  System.out.println("Hello World5");
 
  System.out.println("Hello World6");
@@ -51,6 +52,9 @@ public void gitStash4(){
 
 
 System.out.println("Hello World6");
+
+
+ System.out.println("Hello World4");
 
 }
 
