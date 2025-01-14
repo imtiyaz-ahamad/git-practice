eb857c8f9627a236b198ab4329d40bcf9296dc82
Imtiyaz <imtiyaz.ahamad.work@gmail.com>
2025-01-14 14:03:27 +0530

    now old gitStash will be commit

--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@ class Demo{
  System.out.println("Hello world2");
  System.out.println("Hello World3");
  System.out.println("Hello World4");
+<<<<<<< Updated upstream
  System.out.println("Hello World5");
  System.out.println("Hello World6");
  System.out.println("Hello World7");
@@ -10,5 +11,18 @@ class Demo{
 
         System.out.println("Hello World");
     }
+=======
+
+public void gitStash(){
+    System.out.println("you are commiting all time so when start with new logic in same code to till you have not commit that you can send into temp. area");
+
+}
+public void gitNonStash(){
+System.out.println("this will we commit after git stash");
+
+}
+ 
+
+>>>>>>> Stashed changes
 }
 
