b96c576999dda6a56f30ec7b1a05fdf27cc4f733
Imtiyaz <imtiyaz.ahamad.work@gmail.com>
2025-01-14 18:37:45 +0530

    Revert "Merge pull request #3 from imtiyaz-ahamad/develop"
    This reverts commit e7535132338b05b53c2333f1688c24eaf77d12f3, reversing
    changes made to 3ba38ed2eb3a8463d91a6f12d3db1e2dea5a6fe2.

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ class Demo{
  System.out.println("Hello world2");
  System.out.println("Hello World3");
  System.out.println("Hello World4");
-<<<<<<< HEAD
+
 
  System.out.println("Hello World5");
 
@@ -57,8 +57,9 @@ System.out.println("Hello World6");
 
  System.out.println("Hello World4");
 
-=======
+
  System.out.println("Hello World4");
->>>>>>> parent of e753513 (Merge pull request #3 from imtiyaz-ahamad/develop)
-}
+
+ System.out.println("Hello World4");
+
 
